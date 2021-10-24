5b0bada1372689e6a208469cb64f75b5a5a557bc
MansoorCM <manzoorcm96@gmail.com>
2021-10-24 12:18:12 +0530

    created the express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello world');
+import express from 'express';
+import routes from './routes';
+
+const app = express()
+const port = 3000
+app.use('/api',routes)
+
+app.listen(port, () => {
+    console.log(`server started at port ${port}`);
+})
